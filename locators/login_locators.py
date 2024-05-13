41ad318cc087da914869fd977d56bd940cd329b0
Roman <impyls95@mail.ru>
2024-05-14 01:23:16 +0300

    add project from 1 review from Denis

---
+++ locators/login_locators.py
@@ -0,0 +1,4 @@
+class LoginLocators:
+    USER_NAME = ('css selector', 'input[data-test="username"]')
+    PASSWORD = ('css selector', 'input[data-test="password"]')
+    LOGIN = ('css selector', 'input[data-test="login-button"]')
